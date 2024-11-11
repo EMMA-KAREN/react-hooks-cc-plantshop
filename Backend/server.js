d51ebd9bf180e55fb1d5ce7ea0fb426b55982325
EMMA KAREN <mumokaren26@gmail.com>
2024-11-11 08:53:33 +0300

    changes

---
+++ Backend/server.js
@@ -0,0 +1,8 @@
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use('/api', router)
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running')
+})
